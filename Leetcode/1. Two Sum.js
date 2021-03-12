90069c3c2bf1c8e87bf50cffe2b8ce588775c612
Anqi Lu <anqi0103@gmail.com>
2021-03-11 21:45:04 -0800

    Update 1. Two Sum.js

--- Leetcode/1. Two Sum.js
+++ Leetcode/1. Two Sum.js
@@ -1,3 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// Array && HashTable
+// Map object holds key-value pair
+// Map.has();
+// Map.get();
+// Map.set();
+
 var twoSum = (nums, target) => {
     const dict = new Map();
     for (let i = 0; i < nums.length; i++) {
@@ -8,3 +20,6 @@ var twoSum = (nums, target) => {
         dict.set(nums[i], i);
     }
 };
+
+// Traverse over the array containing n element only once, Time Complexity is O(n);
+// Space Complexity is O(n);
