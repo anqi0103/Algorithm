c0b8d06d3bb26a07bee6f87390fb68abda657238
Anqi Lu <anqi0103@gmail.com>
2021-03-18 11:07:24 -0700

    Create 445. Add Two Numbers II.js

---
+++ Leetcode/445. Add Two Numbers II.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+var reverseList = function(head){
+
+}
+
+var addTwoNumbers = function(l1, l2) {
+    l1 = reverseList(l1);
+    l2 = reverseList(l2);
+  
+    let head = null;
+    let carry = 0;
+  
+}
