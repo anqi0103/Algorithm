723ae0b92167e66d4c8eda406df1b4cea2919dae
Anqi Lu <anqi0103@gmail.com>
2021-05-14 13:34:17 -0700

    Create 0018. 4Sum.java

---
+++ Leetcode-Java/0018. 4Sum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new LinkedList<>();
+        
+        if (nums.length < 4) {
+            return res;
+        }
+        Arrays.sort(nums);
+        
+        
+    }
+}
