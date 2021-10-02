e1f72c9b66d4241809742d30cb878706b8293597
Anqi Lu <anqi0103@gmail.com>
2021-10-01 17:33:44 -0700

    Create 0206. Reverse Linked List.java

---
+++ Leetcode-Java/0206. Reverse Linked List.java
@@ -0,0 +1,32 @@
+// Iterative Approach:
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode dummyHead = null;
+        ListNode temp = head;
+        
+        while (temp != null) {
+            ListNode curr = temp.next;
+            temp.next = prev;
+            dummyHead = temp;
+            temp = curr;
+        }
+        return dummyHead;
+    }
+}
+// Time Complexity: O(n);
+// Space Complexity: O(1);
+
+// Recursive Approach:
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null && head.next == null) {
+            return head;
+        }
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
+// Time Complexity: O(n);
+// Space Complexity: O(n);
